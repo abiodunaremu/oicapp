dc7e89fe5bf075af8cd98d47e649212a73665c1e
Abiodun Aremu <abiodun.milliscript@gmail.com>
2022-10-20 19:31:48 +0100

    New setup

---
+++ queries.js
@@ -0,0 +1,77 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: 'aljyqvfyyiwhxm',
+  host: 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
+  database: 'dc8nk424fhr3sl',
+  password: 'b3f10d4a4e617c4efd0991d156697b7601427dd1dc42e81305dd94af07406fc0',
+  port: 5432,
+});
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createUser = (request, response) => {
+  const { name, email } = request.body;
+
+  pool.query(
+    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+    [name, email],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    }
+  );
+};
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, email } = request.body;
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
